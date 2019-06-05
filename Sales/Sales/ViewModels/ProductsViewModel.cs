02526c382fce5d1ae3f31216fa9827ac8e188a3d
Samuel Serrano Escorcia <41401143+SamuelSerrano@users.noreply.github.com>
2019-06-05 11:46:19 -0500

    Part 04 - 05
    Part 04 - 05

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.ViewModels
+{
+	using System.Collections.ObjectModel;
+	using Common.Models;
+	public class ProductsViewModel : BaseViewModel
+    {
+		private ObservableCollection<Product> products;
+		public ObservableCollection<Product> Products {
+			get { return this.products; }
+			set { this.SetValue(ref this.products, value); }
+		}
+	}
+}
