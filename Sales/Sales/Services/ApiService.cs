d26ab7b9a0e0a1b4948f59f24f0fc6b7d761d590
Samuel Serrano Escorcia <41401143+SamuelSerrano@users.noreply.github.com>
2019-06-06 12:44:42 -0500

    Part07-08

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace Sales.Services
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Net.Http;
+	using System.Threading.Tasks;
+	using Newtonsoft.Json;
+	using Sales.Common.Models;
+	public class ApiService
+	{
+		/// <summary>
+		/// Método Genérico que consume cualquier servicios webApi y cualquier lista.
+		/// </summary>
+		/// <typeparam name="T">Generic</typeparam>
+		/// <param name="urlbase">Url del Servicio API</param>
+		/// <param name="prefix">Salse.API</param>
+		/// <param name="controller">Controlador</param>
+		/// <returns></returns>
+		public async Task<Response> GetList<T>(string urlbase, string prefix, string controller)
+		{
+			try
+			{
+				var client = new HttpClient();
+				client.BaseAddress = new Uri(urlbase);
+				var url = $"{prefix}{controller}";
+				var response = await client.GetAsync(url);
+				var answer = await response.Content.ReadAsStringAsync();
+				if (!response.IsSuccessStatusCode)
+				{
+					return new Response
+					{
+						IsSuccess = false,
+						Message = answer
+					};
+				}
+
+				var list = JsonConvert.DeserializeObject<List<T>>(answer);
+				return new Response
+				{
+					IsSuccess = true,
+					Result = list
+				};
+			}
+			catch (Exception Ex)
+			{
+				return new Response
+				{
+					IsSuccess = false,
+					Message = Ex.Message
+				};
+			}
+		}
+	}
+}
--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -1,13 +1,38 @@
 ﻿namespace Sales.ViewModels
 {
+	using System;
+	using System.Collections.Generic;
 	using System.Collections.ObjectModel;
 	using Common.Models;
+	using Sales.Services;
+	using Xamarin.Forms;
+
 	public class ProductsViewModel : BaseViewModel
     {
+		private ApiService apiService;
 		private ObservableCollection<Product> products;
 		public ObservableCollection<Product> Products {
 			get { return this.products; }
 			set { this.SetValue(ref this.products, value); }
+		}
+
+		public ProductsViewModel()
+		{
+			this.apiService = new ApiService();
+			this.LoadProducts();
+		}
+
+		private async void LoadProducts()
+		{
+			var response = await apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Product");
+			if (!response.IsSuccess)
+			{
+				await Application.Current.MainPage.DisplayAlert("Error",response.Message,"Accept");
+				return;
+			}
+
+			var list = (List<Product>)response.Result;
+			this.Products = new ObservableCollection<Product>(list);
 		}
 	}
 }
